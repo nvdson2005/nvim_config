ce92b3253e4d5e64a9c04e48e41bd802263f4b0d
shon <ducsonds2005@gmail.com>
2025-03-20 22:52:27 +0700

    create keymap K for show information (like mouse hover information in vscode) and set <leader>h for get to the hover information location

--- lua/genyonguyen/init.lua
+++ lua/genyonguyen/init.lua
@@ -4,7 +4,12 @@ require("genyonguyen.config.lazy")
 require("genyonguyen.config.tab")
 require("genyonguyen.plugins.copilot")
 
+-- The current <leader> is set to <Space>. If you wanna customize it, you can change it here
 
+-- Keymap to navigate inside the hover window
+vim.api.nvim_set_keymap('n', '<leader>h', '<cmd>wincmd p<CR>', { noremap = true, silent = true })
+--Keymap to show information about the symbol under the cursor
+vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 -- Use <C-BS> to delete a word in insert mode
 vim.api.nvim_set_keymap("i", "<C-BS>", "<C-w>", { noremap = true, silent = true })
 -- Use <C-BS> to delete a word in command mode
